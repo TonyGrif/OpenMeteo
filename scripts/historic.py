43133e5180859e24b24f25c18d3e09e16a311d0d
TonyGrif <TonyGriffin2000@gmail.com>
2025-07-17 17:00:00 -0400

    script(historic): add pandas parsing for data

--- scripts/historic.py
+++ scripts/historic.py
@@ -1,31 +1,48 @@
 """This module provides a script to test the Historic Weather API"""
 
+import sqlite3
 from pprint import pprint
+
+import pandas as pd
 
 from historic import make_request
 
 
 def main() -> None:
     """Main Script and entry point of this module"""
+    hourly_keys = {0: "temperature_2m"}
+
     # TODO: Allow passing this in via CLI
     params = {
         "latitude": 36.85293,
         "longitude": -75.97799,
         "start_date": "2025-07-01",
         "end_date": "2025-07-16",
-        "hourly": [
-            "temperature_2m",
-            "temperature_80m",
-            "temperature_120m",
-            "temperature_180m",
-            "pressure_msl",
-            "surface_pressure",
-            "dew_point_2m",
-        ],
+        "hourly": list(hourly_keys.values()),
     }
 
     res = make_request(params=params)
-    pprint(res)
+
+    hourly = res[0].Hourly()
+    hour_df = {
+        "date": pd.date_range(
+            start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+            end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+            freq=pd.Timedelta(seconds=hourly.Interval()),
+            inclusive="left",
+        )
+    }
+
+    hour_df["temperature_2m"] = hourly.Variables(
+        *[k for k in hourly_keys if hourly_keys[k] == "temperature_2m"]
+    ).ValuesAsNumpy()
+
+    pprint(hour_df["temperature_2m"])
+
+    hour_df = pd.DataFrame(data=hour_df)
+
+    con = sqlite3.connect("temperatures.db")
+    hour_df.to_sql("vb", con)
 
 
 if __name__ == "__main__":
