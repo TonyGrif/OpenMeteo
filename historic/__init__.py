647f1e3e2bf82bc29ca274f5892953a6836ebc4e
TonyGrif <TonyGriffin2000@gmail.com>
2025-07-17 13:58:45 -0400

    histroic(request): create a request method on historical weather api

---
+++ historic/__init__.py
@@ -0,0 +1,5 @@
+"""This module explores OpenMeteo's Historical Weather API"""
+
+from .request import make_request
+
+__all__ = ["make_request"]
---
+++ scripts/historic.py
@@ -0,0 +1,24 @@
+"""This module provides a script to test the Historic Weather API"""
+
+from pprint import pprint
+
+from historic import make_request
+
+
+def main() -> None:
+    """Main Script and entry point of this module"""
+    # TODO: Allow passing this in via CLI
+    params = {
+        "latitude": 36.85293,
+        "longitude": -75.97799,
+        "start_date": "2025-07-01",
+        "end_date": "2025-07-16",
+        "hourly": ["temperature_2m"],
+    }
+
+    res = make_request(params=params)
+    pprint(res)
+
+
+if __name__ == "__main__":
+    main()
